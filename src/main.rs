93c58178a490b0d213d176815e97526d8c0bf341
Nahianether <nahianether3@gmail.com>
2025-05-05 11:59:01 +0600

    main modify

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,28 @@ use screen_record::run::process_screen_recording;
 async fn main() -> Result<()> {
     env_logger::init();
 
-    // loop {
-    //     if let Err(e) = process_screen_recording(
-    //         "fb01503c-0302-4033-9b0b-ab737ae1875f",
-    //         "https://app.trackforce.io/api/TrackerDesktop/AddWebCamEvent",
-    //         "23.98.93.20",
-    //         "50057",
-    //     )
-    //     .await
-    //     {
-    //         eprintln!("Error: {}", e);
-    //     }
-    // }
-    if let Err(e) = process_screen_recording(
-        "fb01503c-0302-4033-9b0b-ab737ae1875f",
-        "https://app.trackforce.io/api/TrackerDesktop/AddWebCamEvent",
-        "23.98.93.20",
-        "50057",
-    )
-    .await
-    {
-        eprintln!("Error: {}", e);
+    loop {
+        if let Err(e) = process_screen_recording(
+            "fb01503c-0302-4033-9b0b-ab737ae1875f",
+            "https://app.trackforce.io/api/TrackerDesktop/AddWebCamEvent",
+            "23.98.93.20",
+            "50057",
+        )
+        .await
+        {
+            eprintln!("Error: {}", e);
+        }
     }
+    // if let Err(e) = process_screen_recording(
+    //     "fb01503c-0302-4033-9b0b-ab737ae1875f",
+    //     "https://app.trackforce.io/api/TrackerDesktop/AddWebCamEvent",
+    //     "23.98.93.20",
+    //     "50057",
+    // )
+    // .await
+    // {
+    //     eprintln!("Error: {}", e);
+    // }
 
-    Ok(())
+    // Ok(())
 }
